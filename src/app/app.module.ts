83233c3ad2d9df3f198c8cee1d2b1d02c650c79b
zhoulin <nwdpx@sina.com>
2017-12-06 14:52:26 +0800

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
+  declarations: [  // 组件、指令、管道
     AppComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [  // 导入公共的模块
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],  // 导入服务
+  bootstrap: [AppComponent]  // 根路径的入口
 })
 export class AppModule { }
